413dd74c910332dba95584c007032e2cf48a3259
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-23 02:55:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ function BoardMember (name, homeState, training) {
    this.approve = function() {
     return "You can do that!"
   }
-     this.approve = function() {
-    return "You can do that!"
+     this.doCharity = function() {
+    return "I like to help people."
+  }
+     this.releasePressStatement = function() {
+    return "You will see great things from Scuber."
   }
 }
 
