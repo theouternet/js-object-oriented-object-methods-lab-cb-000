f0f342683385e6b0ec7502b6f25a059fd1edf46d
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-23 02:51:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function BoardMember (name, homeState, training) {
   this.name = name 
   this.homeState = homeState
   this.training = training
+  this.veto = function() {
+    return "No, I must disagree"
+  }
+  
 }
 
 
